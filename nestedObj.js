ea8fb3ba9806eaaf78c34d90760c8fa66d1b3f0c
kgayathri4 <04gayathri.k@gmail.com>
2025-11-23 19:34:04 +0530

    Add files via upload

---
+++ nestedObj.js
@@ -0,0 +1,13 @@
+let user={
+    name:"Alice",
+    address:{
+        city:"Benguluru",
+        pin:560001,
+        geo:{lat:11.22,lng:77.33}
+    }
+};
+let {address:{city,
+    geo:{lat,lng}}}=user;
+console.log(city);
+console.log(lat);
+console.log(lng);
